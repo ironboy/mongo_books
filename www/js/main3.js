b409106ad6c60ce35ff08459c669e25f53f2ebc0
Thomas Frank <t@clientsmile.se>
2018-03-08 14:51:15 +0100

    test of user handler class

---
+++ www/js/main3.js
@@ -0,0 +1,35 @@
+// Test of the UserHandler class
+$(()=>{
+  $('body').append(`
+    <p><b>Note:</b To try logout, unregister and change - login first!</p>
+    <button>check</button>
+    <button>login</button>
+    <button>logout</button>
+    <button>register</button>
+    <button>unregister</button>
+    <button>change</button>
+    <pre></pre>
+  `);
+
+  $('button').click(async function(){
+    let email, password, propsToChange;
+    let demo = $(this).text();
+    if(['login', 'register'].includes(demo)){
+      email = prompt('Email');
+      password = prompt('Password');
+    }
+    if(demo == 'change'){
+      propsToChange = {password: prompt('Password')};
+    }
+    let result = await UserHandler[demo](
+      propsToChange || email, password
+    );
+    $('pre').text(
+      'Result:\n' + JSON.stringify(result,'','  ') + '\n\n' +
+      'Info:\n' + JSON.stringify(result.info,'','  ')
+    );
+  });
+
+  $('button').first().click();
+
+});
